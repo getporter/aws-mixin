b415d313083b7f27129aafa772469446774996cb
Vaughn Dice <vaughn.dice@fermyon.com>
2021-12-08 13:59:10 -0700

    feat(build.go): update aws installation to use host arch
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- pkg/aws/build_test.go
+++ pkg/aws/build_test.go
@@ -16,7 +16,7 @@ func TestMixin_Build(t *testing.T) {
 	gotOutput := m.TestContext.GetOutput()
 
 	wantOutput := `RUN apt-get update && apt-get install -y --no-install-recommends curl unzip libc6 less groff
-RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip"
 RUN unzip awscliv2.zip
 RUN ./aws/install
 RUN rm -fr awscliv2.zip ./aws
